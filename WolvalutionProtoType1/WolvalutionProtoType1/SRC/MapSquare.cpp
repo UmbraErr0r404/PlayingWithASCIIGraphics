ac739e86fc83f57903ebd4bb322eed07519fb9f8
UmbraErr0r404 <Elizabeth.Harasymiw@gmail.com>
2016-12-29 13:51:22 -0600

    updated mapsquare's initializers

--- WolvalutionProtoType1/WolvalutionProtoType1/SRC/MapSquare.cpp
+++ WolvalutionProtoType1/WolvalutionProtoType1/SRC/MapSquare.cpp
@@ -1,17 +1,14 @@
 #include "MapSquare.hpp"
 
-MapSquare::MapSquare(){
-	walkable = true;
-	myScenario = Scenario();
-	tile = ColoredCharacter(Black, DarkGreen, char(177));
-	myDescription = "A small grassy area.";
+MapSquare::MapSquare():
+	walkable(true), myScenario(Scenario()), 
+	tile(ColoredCharacter(Black, DarkGreen, char(177))),
+	myDescription("A small grassy area."){
 }
 
-MapSquare::MapSquare(Scenario aEvent, ColoredCharacter c, bool canWalk, std::string myDis){
-	walkable = canWalk;
-	tile = c;
-	myScenario = aEvent;
-	myDescription = myDis;
+MapSquare::MapSquare(Scenario aEvent, ColoredCharacter c, bool canWalk, std::string myDif):
+	walkable(canWalk), tile(c), myScenario(aEvent),
+	myDescription(myDif){
 }
 
 bool MapSquare::isWalkable() const{
