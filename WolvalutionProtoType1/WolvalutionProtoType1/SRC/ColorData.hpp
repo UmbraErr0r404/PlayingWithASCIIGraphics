cf5d6c12abe254697925160394d9e0082614aa65
UmbraErr0r404 <Elizabeth.Harasymiw@gmail.com>
2016-12-29 12:03:03 -0600

    corrected formating

--- WolvalutionProtoType1/WolvalutionProtoType1/SRC/ColorData.hpp
+++ WolvalutionProtoType1/WolvalutionProtoType1/SRC/ColorData.hpp
@@ -17,20 +17,21 @@
 #define ColorCode(bg, fg) ((int(bg) * 0x10) + int(fg)) 
 
 enum color{
-	Black       =0x0,
-	DarkBlue    =0x1,
-	DarkGreen   =0x2,
-	DarkCyan    =0x3,
-	DarkRed     =0x4,
-	DarkPurple  =0x5,
-	DarkYellow  =0x6,
-	LightGray   =0x7,
-	Gray        =0x8,
-	Blue        =0x9,
-	Green       =0xA,
-	Cyan        =0xB,
-	Red         =0xC,
-	Purple      =0xD,
-	Yellow      =0xE,
-	White       =0xF};
+	Black       = 0x0,
+	DarkBlue    = 0x1,
+	DarkGreen   = 0x2,
+	DarkCyan    = 0x3,
+	DarkRed     = 0x4,
+	DarkPurple  = 0x5,
+	DarkYellow  = 0x6,
+	LightGray   = 0x7,
+	Gray        = 0x8,
+	Blue        = 0x9,
+	Green       = 0xA,
+	Cyan        = 0xB,
+	Red         = 0xC,
+	Purple      = 0xD,
+	Yellow      = 0xE,
+	White       = 0xF
+};
 #endif 
