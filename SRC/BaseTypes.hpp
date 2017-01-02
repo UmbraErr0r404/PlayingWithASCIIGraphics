92b6505ecaa119665b62c18c9d7cb4e22adeeb4f
unDeadHerbs <undeadherbs@gmail.com>
2017-01-01 19:40:04 -0600

    made type custome so that code is more readable

--- SRC/BaseTypes.hpp
+++ SRC/BaseTypes.hpp
@@ -3,7 +3,52 @@
 
 typedef unsigned int uint;
 
-#include <utility>
-typedef std::pair<uint,uint> coordinate;
+template <auto T=uint>
+class coordinate{
+public:
+	T x,y;
+
+	coordinate():x(0),y(0){}
+	explicit coordinate(T x,T y):x(x),y(y){}
+
+	bool operator==(const coordinate<T>&)const;
+	bool operator>(const coordinate<T>&)const;
+	bool operator<(const coordinate<T>&)const;
+	bool operator>=(const coordinate<T>&)const;
+	bool operator<=(const coordinate<T>&)const;
+	coordinate<T> operator=(const coordinate<T>&);
+};
+
+template<auto T=uint>
+bool coordinate::operator==(const coordinate<T>& rhs)const{
+	return x==rhs.x && y==rhs.y;
+}
+
+template<auto T=uint>
+bool coordinate::operator>(const coordinate<T>& rhs)const{
+	return x>rhs.x && y>rhs.y;
+}
+
+template<auto T=uint>
+bool coordinate::operator<(const coordinate<T>& rhs)const{
+	return x<rhs.x && y<rhs.y;
+}
+
+template<auto T=uint>
+bool coordinate::operator>=(const coordinate<T>& rhs)const{
+	return x>=rhs.x && y>=rhs.y;
+}
+
+template<auto T=uint>
+bool coordinate::operator<=(const coordinate<T>& rhs)const{
+	return x<=rhs.x && y<=rhs.y;
+}
+
+
+template<auto T=uint>
+bool coordinate::operator=(const coordinate<T>& rhs){
+	return x=rhs.x && y=rhs.y;
+}
+
 
 #endif
